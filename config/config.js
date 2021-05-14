803ac4c89084191518ac8a93eb000b9df9d447ac
juliendubois50 <84192370+juliendubois50@users.noreply.github.com>
2021-05-14 17:23:53 +0200

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports.options = {
+    token: 'NjI1MzQwNjczOTgxMTUzMjkw.XYeH4g.5nzrpliqyWQW53ghY1_mvl1YJt8',
+    topicChannels: [
+      'status-du-serveur'
+    ],
+    roles: ["Admin"],
+    servers: [
+        { name: 'California City Rp', url: 'http://188.40.16.78:30797', alias: 'CaliforniaCityRP' },
+    ],
+    pollRate: 30,
+};
